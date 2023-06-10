6d17aff6cc595745de2b7b6414b1802e67cfdfbb
ashbsmith <aislingbsmith@gmail.com>
2023-06-10 11:26:27 +0100

    basic css for topbar

--- ash-website/src/components/Header.js
+++ ash-website/src/components/Header.js
@@ -3,11 +3,13 @@ import './Header.css';
 
 function Header() {
   return (<>
+  <div className="top-bar">
     <div className="name">aisling brogan-smith</div>
       <div className="work">work</div>
       <div className="about">about</div>
       <div className="contact">contact</div>
       <div className="cv">cv</div>
+  </div>
       </>
         );
 };
