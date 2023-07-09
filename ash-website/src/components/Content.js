e60ed0ebfc9e210d8af634f91b822bee9729c906
ashbsmith <aislingbsmith@gmail.com>
2023-07-09 12:33:51 +0100

    name on website

--- ash-website/src/components/Content.js
+++ ash-website/src/components/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Content.css';
 import hackhealth from '../images/hackhealth.png';
+import name from '../images/name.png';
 
 function Content() {
   return (
   <>
+  <img src={name} alt="name-img" className = "name-image"/>
     <div className = "my-work">
       <h2>my work</h2>
         <img src={hackhealth} alt="hackhealth-img" className = "hackhealth-image"/>
