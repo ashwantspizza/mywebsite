682eec605a33588c9b1f4cc715796f72d33f4652
ashbsmith <aislingbsmith@gmail.com>
2023-07-26 21:30:33 +0100

    container around name image

--- ash-website/src/components/Content.js
+++ ash-website/src/components/Content.js
@@ -6,7 +6,9 @@ import name from '../images/name.png';
 function Content() {
   return (
   <>
-  <img src={name} alt="name-img" className = "name-image"/>
+  <div className = "name-image-container">
+    <img src={name} alt="name-img" className = "name-image"/>
+  </div>
     <div className = "my-work">
       <h2>my work</h2>
         <img src={hackhealth} alt="hackhealth-img" className = "hackhealth-image"/>
