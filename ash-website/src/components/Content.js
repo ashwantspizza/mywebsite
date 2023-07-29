e88020d9d7e2b1ceaf09efba588752ff8be3391f
ashbsmith <aislingbrogansmith@gmail.com>
2023-07-29 21:11:30 +0100

    added project link

--- ash-website/src/components/Content.js
+++ ash-website/src/components/Content.js
@@ -20,7 +20,9 @@ function Content() {
         <h3>HackHealth</h3>
       </div>
       <div className = "hackhealth-project-desc">HackHealth is an application built in React that helps you to keep track of your daily steps and your weekly step goals.</div>
-      <div className = "hackhealth-project-link">repo link</div>
+      <div className = "hackhealth-project-link">
+      <a href="https://github.com/SchoolOfCode/bc14_w7_project-frontend-room1-ash-anjum-david-tom">repo link</a>
+      </div>
       </div>
   </div>
     </div>
