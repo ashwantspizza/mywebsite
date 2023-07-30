c8364c19ee9c4826d485d26071780900c6001727
ashbsmith <aislingbrogansmith@gmail.com>
2023-07-30 23:32:47 +0100

    nudge project layout

--- ash-website/src/components/LandingPage.js
+++ ash-website/src/components/LandingPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './LandingPage.css';
-import hackhealth from '../images/hackhealth.png';
 import name from '../images/name.png';
+import hackhealth from '../images/hackhealth.png';
+import nudge from '../images/nudge.png';
 import { HashRouter } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
@@ -34,30 +35,38 @@ function LandingPage() {
           </div>
         </nav>
 
-        <section id="work">
-        <div className="top-bar">
-      <div className = "name-image-container">
+        <div className = "name-image-container">
     <img src={name} alt="name-img" className = "name-image"/>
   </div>
-  <div className = "my-work-section">
-   <div className = "my-work-text">
-        <h2>my work</h2>
-  </div>
-  <div className = "hackhealth-container">
-        <img src={hackhealth} alt="hackhealth-img" className = "hackhealth-image"/>
-        <div className = "hackhealth-project-desc-container">
-      <div className = "hackhealth-project-title">
-        <h3>HackHealth</h3>
-      </div>
-      <div className = "hackhealth-project-desc">HackHealth is an application built in React that helps you to keep track of your daily steps and your weekly step goals.</div>
-      <div className = "hackhealth-project-link">
-      <a href="https://github.com/SchoolOfCode/bc14_w7_project-frontend-room1-ash-anjum-david-tom">repo link</a>
-      </div>
-      </div>
-  </div>
+
+  <section id="work">
+  <div className="my-work-section">
+    <div className="my-work-text">
+      <h2>my work</h2>
     </div>
+    <div className="hackhealth-container">
+      <img src={hackhealth} alt="hackhealth-img" className="hackhealth-image" />
+      <div className="hackhealth-project-desc-container">
+        <div className="hackhealth-project-title"> <h3>HackHealth</h3> </div>
+        <div className="hackhealth-project-desc">HackHealth is an application built in React that helps you to keep track of your daily steps and your weekly step goals.</div>
+        <div className="hackhealth-project-link">
+          <a href="https://github.com/SchoolOfCode/bc14_w7_project-frontend-room1-ash-anjum-david-tom">repo link</a>
+        </div>
+      </div>
     </div>
-        </section>
+    <div className="nudge-container">
+      <img src={nudge} alt="nudge-img" className="nudge-image" />
+      <div className="nudge-project-desc-container">
+        <div className="nudge-project-title"> <h3>Nudge</h3> </div>
+        <div className="nudge-project-desc">Nudge is an application built in React that ...</div>
+        <div className="nudge-project-link">
+          <a href="https://8bit-nudge.netlify.app/">app link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
         <section id="about">
           {/* Content for the "about" section goes here */}
