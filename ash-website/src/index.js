922ee4b05b04fd71aebbabccbdb563b2f15a411c
ashbsmith <aislingbrogansmith@gmail.com>
2023-10-18 17:10:19 +0100

    starting to implement backend for contact form

--- ash-website/src/index.js
+++ ash-website/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjRfjPFLAzg1AI28l_PqOdQTKvp5E1HAk",
+  authDomain: "my-website-abs.firebaseapp.com",
+  projectId: "my-website-abs",
+  storageBucket: "my-website-abs.appspot.com",
+  messagingSenderId: "1008582978239",
+  appId: "1:1008582978239:web:cbcfe7a68e89b317a0b40a",
+  measurementId: "G-06LDJ6SWXG"
+};
+
+//initialising firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
